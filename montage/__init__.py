c0bf56247fd06bfa597870301094d4e75a6e2709
astrofrog <thomas.robitaille@gmail.com>
2009-12-30 21:46:08 +0000

    0.9.0 release
    git-svn-id: https://python-montage.svn.sourceforge.net/svnroot/python-montage/trunk@15 cf66f5ca-ee26-4772-92ff-0de9cccd0768

--- montage/__init__.py
+++ montage/__init__.py
@@ -1,3 +1,19 @@
+# A Python wrapper for the Montage mosaicking software
+# Copyright (C) 2009 Thomas P. Robitaille
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from commands import *
 from wrappers import *
 
