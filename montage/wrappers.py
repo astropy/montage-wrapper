ecbdaf0ce3fdea32992ffa72255f2a2ec0346e02
astrofrog <thomas.robitaille@gmail.com>
2009-12-30 20:29:23 +0000

    Updated wrappers to not change directory
    git-svn-id: https://python-montage.svn.sourceforge.net/svnroot/python-montage/trunk@13 cf66f5ca-ee26-4772-92ff-0de9cccd0768

--- montage/wrappers.py
+++ montage/wrappers.py
@@ -1,4 +1,5 @@
 import os
+import shutil as sh
 import random
 import string
 
@@ -17,7 +18,7 @@ def _make_work_dir():
     work_dir = "/tmp/pymontage/"+random_id
     os.mkdir(work_dir)
 
-    return work_dir
+    return work_dir + "/"
 
 try:
 
@@ -91,44 +92,33 @@ def reproject(in_image, out_image, bitpix=None, north_aligned=False,
             Drizzle factor (see mProject)
     '''
 
-    # Remember start directory
-    start_dir = os.path.abspath('.')
-
-    # Find path to input file
+    # Find path to input and output file
     in_image = os.path.abspath(in_image)
     out_image = os.path.abspath(out_image)
 
     # Make work directory
     work_dir = _make_work_dir()
 
-    # Go to work directory
-    os.chdir(work_dir)
-
     # Create raw directory
-    os.mkdir('raw')
-    os.mkdir('final')
+    os.mkdir(work_dir + 'raw')
+    os.mkdir(work_dir + 'final')
 
     # Link to image
-    os.symlink(in_image, 'raw/image.fits')
+    os.symlink(in_image, work_dir + 'raw/image.fits')
 
     # Make image table
-    m.mImgtbl('raw', 'images_raw.tbl', corners=True)
+    m.mImgtbl(work_dir + 'raw', work_dir + 'images_raw.tbl', corners=True)
 
     # Make new north-aligned header
-    m.mMakeHdr('images_raw.tbl', 'header.hdr', north_aligned=north_aligned,
+    m.mMakeHdr(work_dir + 'images_raw.tbl', work_dir + 'header.hdr', north_aligned=north_aligned,
         system=system, equinox=equinox)
 
     try:
-        m.mProjectPP('raw/image.fits', 'final/image.fits', 'header.hdr')
+        m.mProjectPP(work_dir + 'raw/image.fits', work_dir + 'final/image.fits', work_dir + 'header.hdr')
     except MontageError:
-        m.mProject('raw/image.fits', 'final/image.fits', 'header.hdr')
+        m.mProject(work_dir + 'raw/image.fits', work_dir + 'final/image.fits', work_dir + 'header.hdr')
 
-    m.mConvert('final/image.fits', 'final/image_b.fits', bitpix=bitpix)
-
-    os.rename('final/image_b.fits', out_image)
-
-    # Go to start directory
-    os.chdir(start_dir)
+    m.mConvert(work_dir + 'final/image.fits', out_image, bitpix=bitpix)
 
     if cleanup:
         # Deleting work directory
@@ -143,18 +133,23 @@ def reproject(in_image, out_image, bitpix=None, north_aligned=False,
 
 def mosaic(input_dir, output_dir, header=None, mpi=False, n_proc=8,
     background_match=False, imglist=None, combine="mean", exact_size=False,
-    cleanup=True):
+    cleanup=True, bitpix=-32):
 
     assert combine=='mean' or combine=='median' or combine=='count', \
             "combine should be one of mean/median/count"
 
-    # Find absolute path to files
-    start_dir = os.path.abspath('.')
-    input_dir = os.path.abspath(input_dir)
+    # Find path to input and output directory
+    input_dir = os.path.abspath(input_dir) + "/"
+    output_dir = os.path.abspath(output_dir) + "/"
+
+    # Make work directory
     work_dir = _make_work_dir()
-    output_dir = os.path.abspath(output_dir)
+
+    # Find path to header file if specified
     if header:
         header = os.path.abspath(header)
+
+    # Find path to image list if specified
     if imglist:
         imglist = os.path.abspath(imglist)
 
@@ -164,86 +159,81 @@ def mosaic(input_dir, output_dir, header=None, mpi=False, n_proc=8,
     else:
         os.mkdir(output_dir)
 
-    # Go to work directory
-    os.chdir(work_dir)
-
     # Create symbolic links
-    os.symlink(input_dir, 'raw')
-    os.symlink(output_dir, 'final')
+    os.symlink(input_dir, work_dir + 'raw')
+
     if header:
-        os.symlink(header, 'header.hdr')
+        os.symlink(header, work_dir + 'header.hdr')
+
     if imglist:
-        os.symlink(imglist, 'imglist')
+        os.symlink(imglist, work_dir + 'imglist')
 
     # Create temporary directories for Montage
-    os.mkdir('projected')
+    os.mkdir(work_dir + 'projected')
     if background_match:
-        os.mkdir('diffs')
-        os.mkdir('corrected')
+        os.mkdir(work_dir + 'diffs')
+        os.mkdir(work_dir + 'corrected')
 
     # List frames to mosaic
     print "Listing raw frames"
-    m.mImgtbl('raw', 'images_raw.tbl', img_list=imglist)
+    m.mImgtbl(work_dir + 'raw', work_dir + 'images_raw.tbl', img_list=imglist)
 
     # Compute header if needed
     if not header:
         print "Computing optimal header"
-        m.mMakeHdr('images_raw.tbl', 'header.hdr')
+        m.mMakeHdr(work_dir + 'images_raw.tbl', work_dir + 'header.hdr')
 
 
     # Projecting raw frames
     print "Projecting raw frames"
-    m.mProjExec('images_raw.tbl', 'header.hdr', 'projected', 'stats.tbl',
-                    raw_dir='raw', mpi=mpi, n_proc=n_proc)
+    m.mProjExec(work_dir + 'images_raw.tbl', work_dir + 'header.hdr', work_dir + 'projected', work_dir + 'stats.tbl',
+                    raw_dir=work_dir + 'raw', mpi=mpi, n_proc=n_proc)
 
     # List projected frames
-    m.mImgtbl('projected', 'images_projected.tbl')
+    m.mImgtbl(work_dir + 'projected', work_dir + 'images_projected.tbl')
 
     if background_match:
 
         # Modeling background
 
         print "Modeling background"
-        m.mOverlaps('images_projected.tbl', 'diffs.tbl')
-        m.mDiffExec('diffs.tbl', 'header.hdr', 'diffs', proj_dir='projected',
+        m.mOverlaps(work_dir + 'images_projected.tbl', work_dir + 'diffs.tbl')
+        m.mDiffExec(work_dir + 'diffs.tbl', work_dir + 'header.hdr', work_dir + 'diffs', proj_dir=work_dir + 'projected',
                     mpi=mpi, n_proc=n_proc)
-        m.mFitExec('diffs.tbl', 'fits.tbl', 'diffs')
-        m.mBgModel('images_projected.tbl', 'fits.tbl', 'corrections.tbl',
+        m.mFitExec(work_dir + 'diffs.tbl', work_dir + 'fits.tbl', work_dir + 'diffs')
+        m.mBgModel(work_dir + 'images_projected.tbl', work_dir + 'fits.tbl', work_dir + 'corrections.tbl',
                     n_iter=32767, level_only=True)
 
         # Matching background
         print "Matching background"
-        m.mBgExec('images_projected.tbl', 'corrections.tbl', 'corrected',
-                    proj_dir='projected')
-        os.system('cp corrections.tbl final/')
+        m.mBgExec(work_dir + 'images_projected.tbl', work_dir + 'corrections.tbl', work_dir + 'corrected',
+                    proj_dir=work_dir + 'projected')
+        sh.copy(work_dir + 'corrections.tbl', output_dir)
 
         # Mosaicking frames
         print "Mosaicking BCD frames"
 
-        m.mImgtbl('corrected', 'images_corrected.tbl')
-        m.mAdd('images_corrected.tbl', 'header.hdr', 'final/mosaic64.fits',
-                    img_dir='corrected', type=combine, exact=exact_size)
-        os.system('cp images_projected.tbl final/')
-        os.system('cp images_corrected.tbl final/')
+        m.mImgtbl(work_dir + 'corrected', work_dir + 'images_corrected.tbl')
+        m.mAdd(work_dir + 'images_corrected.tbl', work_dir + 'header.hdr', output_dir + 'mosaic64.fits',
+                    img_dir=work_dir + 'corrected', type=combine, exact=exact_size)
+        sh.copy(work_dir + 'images_projected.tbl', output_dir)
+        sh.copy(work_dir + 'images_corrected.tbl', output_dir)
 
     else:
 
         # Mosaicking frames
         print "Mosaicking BCD frames"
 
-        m.mAdd('images_projected.tbl', 'header.hdr', 'final/mosaic64.fits',
-                    img_dir='projected', type=combine, exact=exact_size)
-        os.system('cp images_projected.tbl final/')
+        m.mAdd(work_dir + 'images_projected.tbl', work_dir + 'header.hdr', output_dir + 'mosaic64.fits',
+                    img_dir=work_dir + 'projected', type=combine, exact=exact_size)
+        sh.copy(work_dir + 'images_projected.tbl', output_dir)
 
-    m.mConvert('final/mosaic64.fits', 'final/mosaic.fits', bitpix=-32)
-    m.mConvert('final/mosaic64_area.fits', 'final/mosaic_area.fits',
-                bitpix=-32)
+    m.mConvert(output_dir + 'mosaic64.fits', output_dir + 'mosaic.fits', bitpix=bitpix)
+    m.mConvert(output_dir + 'mosaic64_area.fits', output_dir + 'mosaic_area.fits',
+                bitpix=bitpix)
 
-    os.remove("final/mosaic64.fits")
-    os.remove("final/mosaic64_area.fits")
-
-    # Go to start directory
-    os.chdir(start_dir)
+    os.remove(output_dir + "mosaic64.fits")
+    os.remove(output_dir + "mosaic64_area.fits")
 
     if cleanup:
         # Deleting work directory
