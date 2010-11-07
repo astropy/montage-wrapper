65f0ef0ae523059db6ecf3856c9e2695cad81d38
Thomas Robitaille <thomas.robitaille@gmail.com>
2010-11-07 07:52:36 -0500

    Check whether Montage commands are available

--- montage/__init__.py
+++ montage/__init__.py
@@ -2,3 +2,13 @@ from commands import *
 from wrappers import *
 
 __version__ = '0.9.1'
+
+# Check whether Montage is installed
+installed = False
+for dir in os.environ['PATH'].split(':'):
+    if os.path.exists(dir + '/mProject'):
+        installed = True
+        break
+
+if not installed:
+    raise Exception("Montage commands are not in your PATH")
