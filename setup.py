32558e5e86a092dcf4c8a140d271a297e947f1fa
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-01-24 16:29:40 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,24 @@
 
 from distutils.core import setup
 
+try:  # Python 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:  # Python 2.x
+    from distutils.command.build_py import build_py
+
 setup(name='python-montage',
       version='0.9.3',
       description='python-montage - a python Montage wrapper',
       author='Thomas Robitaille',
-      author_email='robitaille@users.sourceforge.net',
-      url='http://python-montage.sourceforge.net/',
+      author_email='thomas.robitaille@gmail.com',
+      url='http://astrofrog.github.com/python-montage/',
       packages=['montage'],
+      provides=['montage'],
+      cmdclass={'build_py': build_py},
+      keywords=['Scientific/Engineering'],
+      classifiers=[
+                   "Development Status :: 4 - Beta",
+                   "Programming Language :: Python",
+                   "License :: OSI Approved :: MIT License",
+                  ],
      )
