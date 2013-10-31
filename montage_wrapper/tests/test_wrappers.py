15e189d54010f9921113ea319f42a5fb44c12e1f
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-10-31 10:50:06 +0100

    Don't use hash to compare resulting mosaics, not stable

--- montage_wrapper/tests/test_wrappers.py
+++ montage_wrapper/tests/test_wrappers.py
@@ -1,8 +1,10 @@
 import os
+import shutil
 import tempfile
 from hashlib import md5
 
 import numpy as np
+from numpy.testing import assert_allclose
 from astropy.wcs import WCS
 from astropy.io import fits
 
@@ -38,7 +40,26 @@ class TestMosaic(object):
                 hdu.data = np.random.random((100,100))
                 hdu.writeto(os.path.join(self.tmpdir, 'raw', 'test_{0:02d}_{1:02d}.fits'.format(i, j)), clobber=True)
 
+    def teardown_class(self):
+        shutil.rmtree(self.tmpdir)
+
     def test_mosaic(self):
         mosaic(os.path.join(self.tmpdir, 'raw'),os.path.join(self.tmpdir, 'mosaic'))
         hdu = fits.open(os.path.join(self.tmpdir, 'mosaic', 'mosaic.fits'))[0]
-        assert md5(hdu.data).hexdigest() == "3377373e039b3f934262151e82beaaf1"
+        assert hdu.data.shape == (288, 282)
+        valid = hdu.data[~np.isnan(hdu.data)]
+        assert len(valid) == 65029
+        assert_allclose(np.std(valid), 0.12658458001333581)
+        assert_allclose(np.mean(valid), 0.4995945318627074)
+        assert_allclose(np.median(valid), 0.5003376603126526)
+
+    def test_mosaic_background_match(self):
+        mosaic(os.path.join(self.tmpdir, 'raw'),os.path.join(self.tmpdir, 'mosaic_bkgmatch'), background_match=True)
+        hdu = fits.open(os.path.join(self.tmpdir, 'mosaic_bkgmatch', 'mosaic.fits'))[0]
+        assert hdu.data.shape == (288, 282)
+        valid = hdu.data[~np.isnan(hdu.data)]
+        assert len(valid) == 65029
+        assert_allclose(np.std(valid), 0.12661606622654725)
+        assert_allclose(np.mean(valid), 0.4994805202294361)
+        assert_allclose(np.median(valid), 0.5002447366714478)
+
