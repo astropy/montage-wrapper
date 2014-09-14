fda5724baeb5f46ddb3a0c6bc4e6a637c6382744
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-09-14 10:19:23 +0200

    Updated to v0.4 of the Astropy package template

--- montage_wrapper/__init__.py
+++ montage_wrapper/__init__.py
@@ -1,156 +1,56 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from __future__ import print_function
+"""
+This is an Astropy affiliated package.
+"""
 
-#this indicates whether or not we are in the package's setup.py
-try:
-    _ASTROPY_SETUP_
-except NameError:
-    from sys import version_info
-    if version_info[0] >= 3:
-        import builtins
-    else:
-        import __builtin__ as builtins
-    builtins._ASTROPY_SETUP_ = False
-    del version_info
-    del builtins
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
 
-try:
-    from .version import version as __version__
-except ImportError:
-    __version__ = ''
-try:
-    from .version import githash as __githash__
-except ImportError:
-    __githash__ = ''
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
 
-# set up the test command
+    from .commands import *
+    from .wrappers import *
+    from .mpi import set_mpi_command
 
+    # Check whether Montage is installed
+    installed = False
+    for dir in os.environ['PATH'].split(':'):
+        if os.path.exists(dir + '/mProject'):
+            installed = True
+            break
 
-def _get_test_runner():
-    from astropy.tests.helper import TestRunner
-    return TestRunner(__path__[0])
+    import textwrap
 
+    error_wrap = textwrap.TextWrapper(initial_indent=" " * 11,
+                                      subsequent_indent=" " * 11,
+                                      width=72)
 
-def test(package=None, test_path=None, args=None, plugins=None,
-         verbose=False, pastebin=None, remote_data=False, pep8=False,
-         pdb=False, coverage=False, open_files=False, **kwargs):
-    """
-    Run the tests using py.test. A proper set of arguments is constructed and
-    passed to `pytest.main`.
+    MONTAGE_MISSING = """
+    ERROR: Montage commands could not be found.
 
-    Parameters
-    ----------
-    package : str, optional
-        The name of a specific package to test, e.g. 'io.fits' or 'utils'.
-        If nothing is specified all default tests are run.
+    In order to use the montage_wrapper module, you will first need to
+    install the IPAC Montage software from:
 
-    test_path : str, optional
-        Specify location to test by path. May be a single file or
-        directory. Must be specified absolutely or relative to the
-        calling directory.
+        http://montage.ipac.caltech.edu
 
-    args : str, optional
-        Additional arguments to be passed to `pytest.main` in the `args`
-        keyword argument.
+    and ensure that the Montage commands (e.g. mAdd, mProject, etc.) are in
+    your $PATH. Your current $PATH variable contains the following paths,
+    but none of them contain the Montage commands:
 
-    plugins : list, optional
-        Plugins to be passed to `pytest.main` in the `plugins` keyword
-        argument.
+        PATH = {path}
 
-    verbose : bool, optional
-        Convenience option to turn on verbose output from py.test. Passing
-        True is the same as specifying `-v` in `args`.
+    If the Montage commands are in one of these directories, then please
+    report this as an issue with montage-wrapper.
+    """.format(path=error_wrap.fill(os.environ['PATH']).strip())
 
-    pastebin : {'failed','all',None}, optional
-        Convenience option for turning on py.test pastebin output. Set to
-        'failed' to upload info for failed tests, or 'all' to upload info
-        for all tests.
+    ON_RTD = os.environ.get('READTHEDOCS', None) == 'True'
 
-    remote_data : bool, optional
-        Controls whether to run tests marked with @remote_data. These
-        tests use online data and are not run by default. Set to True to
-        run these tests.
-
-    pep8 : bool, optional
-        Turn on PEP8 checking via the pytest-pep8 plugin and disable normal
-        tests. Same as specifying `--pep8 -k pep8` in `args`.
-
-    pdb : bool, optional
-        Turn on PDB post-mortem analysis for failing tests. Same as
-        specifying `--pdb` in `args`.
-
-    coverage : bool, optional
-        Generate a test coverage report.  The result will be placed in
-        the directory htmlcov.
-
-    open_files : bool, optional
-        Fail when any tests leave files open.  Off by default, because
-        this adds extra run time to the test suite.  Works only on
-        platforms with a working `lsof` command.
-
-    parallel : int, optional
-        When provided, run the tests in parallel on the specified
-        number of CPUs.  If parallel is negative, it will use the all
-        the cores on the machine.  Requires the `pytest-xdist` plugin
-        is installed. Only available when using Astropy 0.3 or later.
-
-    kwargs
-        Any additional keywords passed into this function will be passed
-        on to the astropy test runner.  This allows use of test-related
-        functionality implemented in later versions of astropy without
-        explicitly updating the package template.
-
-    See Also
-    --------
-    pytest.main : py.test function wrapped by `run_tests`.
-
-    """
-    test_runner = _get_test_runner()
-    return test_runner.run_tests(
-        package=package, test_path=test_path, args=args,
-        plugins=plugins, verbose=verbose, pastebin=pastebin,
-        remote_data=remote_data, pep8=pep8, pdb=pdb,
-        coverage=coverage, open_files=open_files, **kwargs)
-
-from .commands import *
-from .wrappers import *
-from .mpi import set_mpi_command
-
-# Check whether Montage is installed
-installed = False
-for dir in os.environ['PATH'].split(':'):
-    if os.path.exists(dir + '/mProject'):
-        installed = True
-        break
-
-import textwrap
-
-error_wrap = textwrap.TextWrapper(initial_indent=" " * 11,
-                                  subsequent_indent=" " * 11,
-                                  width=72)
-
-MONTAGE_MISSING = """
-ERROR: Montage commands could not be found.
-
-In order to use the montage_wrapper module, you will first need to
-install the IPAC Montage software from:
-
-    http://montage.ipac.caltech.edu
-
-and ensure that the Montage commands (e.g. mAdd, mProject, etc.) are in
-your $PATH. Your current $PATH variable contains the following paths,
-but none of them contain the Montage commands:
-
-    PATH = {path}
-
-If the Montage commands are in one of these directories, then please
-report this as an issue with montage-wrapper.
-""".format(path=error_wrap.fill(os.environ['PATH']).strip())
-
-ON_RTD = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not _ASTROPY_SETUP_ and not ON_RTD and not installed:
-    print(MONTAGE_MISSING)
-    import sys
-    sys.exit(1)
+    if not ON_RTD and not installed:
+        print(MONTAGE_MISSING)
+        import sys
+        sys.exit(1)
